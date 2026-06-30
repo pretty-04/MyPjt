99f6eec7d7a2071cdfa155285f2b241f0584eac0
honeyjose <honey@vjcet.org>
2026-06-30 15:51:58 +0530

    updated

---
+++ Sheena/welcome.py
@@ -0,0 +1,8 @@
+def add_numbers():
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    sum_result = a + b
+    print("The sum is:", sum_result)
+
+
+add_numbers()
